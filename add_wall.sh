9ef485ffef1ebee103e7691a9fe1e09a2b44b3e3
Alex <sashapisarchuk@gmail.com>
2024-11-13 01:45:32 +0200

    deleted .sh file

--- add_wall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the path to the images folder
-image_folder="./walls"
-
-# Create the markdown content for each image in the folder
-for img in $image_folder/*; do
-  echo "![$(basename "$img")]($img)" >>README.md
-done
