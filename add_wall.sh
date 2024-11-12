35ddea77748ef553068de2e9778953ec756e6342
Alex <sashapisarchuk@gmail.com>
2024-11-13 01:27:20 +0200

    another commit

---
+++ add_wall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set the path to the images folder
+image_folder="./"
+
+# Create the markdown content for each image in the folder
+for img in $image_folder/*; do
+  echo "![$(basename "$img")]($img)" >>README.md
+done
