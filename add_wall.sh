7a75fd33a2b98700ebb49ebd7eebaa5942b62ea1
Alex <sashapisarchuk@gmail.com>
2024-11-13 01:33:37 +0200

    another fix

--- add_wall.sh
+++ add_wall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the path to the images folder
-image_folder="./"
+image_folder="./walls"
 
 # Create the markdown content for each image in the folder
 for img in $image_folder/*; do
