db41297a60f42750ab8511735a10937b5af86eba
Alex <sashapisarchuk@gmail.com>
2024-11-13 02:05:19 +0200

    sth

--- add_w.py
+++ add_w.py
@@ -12,14 +12,13 @@ with open(readme_file, 'a') as f:
         if image_name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.svg', '.bmp', '.webp', '.tiff')):
             image_path = os.path.join(image_folder, image_name)
 
-            # Directly use the image file path for the markdown image link
             row_images.append(f"![{image_name}]({image_path})")
 
             if len(row_images) == 5:  
                 f.write("  ".join(row_images) + "\n\n")
                 row_images = []
 
-    if row_images:  
+    if row_images:
         f.write("  ".join(row_images) + "\n\n")
 
 print("README.md updated with images.")
