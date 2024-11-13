f7cfbfa89958d1379ec15f8e49cc3038a9ad724c
Alex <sashapisarchuk@gmail.com>
2024-11-13 02:06:42 +0200

    sth

--- add_w.py
+++ add_w.py
@@ -5,6 +5,9 @@ readme_file = "README.md"
 
 with open(readme_file, 'a') as f:
     f.write("\n# Wallpaper Gallery\n\n")
+    
+    # Start the table for the image grid
+    f.write("<table>\n")
 
     row_images = []
 
@@ -12,14 +15,20 @@ with open(readme_file, 'a') as f:
         if image_name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.svg', '.bmp', '.webp', '.tiff')):
             image_path = os.path.join(image_folder, image_name)
 
-            row_images.append(f"![{image_name}]({image_path})")
+            # Add the image as an HTML <img> tag in a table cell <td>
+            row_images.append(f"<td><img src='{image_path}' alt='{image_name}' width='150' height='150'></td>")
 
-            if len(row_images) == 5:  
-                f.write("  ".join(row_images) + "\n\n")
-                row_images = []
+            # After 5 images, close the row and start a new one
+            if len(row_images) == 5:
+                f.write("<tr>\n" + "\n".join(row_images) + "\n</tr>\n")
+                row_images = []  # Reset for the next row
 
+    # If there are any remaining images that didn't fill a full row
     if row_images:
-        f.write("  ".join(row_images) + "\n\n")
+        f.write("<tr>\n" + "\n".join(row_images) + "\n</tr>\n")
 
-print("README.md updated with images.")
+    # Close the table
+    f.write("</table>\n")
+
+print("README.md updated with images in a grid layout.")
 
